e8d29ede8cd33a29c2cd166c35293ed777100ddb
ashikprodhan <abubakarsiddik652@gmail.com>
2021-04-02 18:33:40 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
+var cors = require('cors')
+var bodyParser = require('body-parser')
+const ObjectID =require('mongodb').ObjectID;
+
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000;
+app.use(cors())
+app.use(bodyParser.json())
+// console.log(process.env.DB_USER);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+/* for mongodb connection  */
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3tdcv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    console.log('error connection',err);
+    
+  const productCollection = client.db("freshValley").collection("products");
+  const orderCollection = client.db("freshValley").collection("orders");
+//   console.log('database connected succesfully');
+app.post('/addOrder',(req,res)=>{
+    const newOrder =req.body;
+    orderCollection.insertOne(newOrder)
+    .then(result =>{
+        res.send(result.insertedCount > 0);
+    })
+    console.log('newOrder',newOrder);
+})
+
+
+
+app.get('/orders',(req,res)=>{
+    // console.log(req.query.email);
+    orderCollection.find({email:req.query.email})
+    .toArray((err,documents)=>{
+        res.send(documents);
+    })
+})
+
+
+app.get('/products',(req,res)=>{
+    productCollection.find()
+    .toArray((err,items) =>{
+        res.send(items)
+        // console.log('items from',items);
+    })
+})
+app.get('/product/:id',(req,res)=>{
+    const id=ObjectId(req.params.id);
+    console.log('find this',id);
+
+    productCollection.find({_id: req.params.id})
+    .toArray((err,documents) =>{
+        res.send(documents)
+        console.log('documents from',documents);
+    })
+})
+app.post('/addProduct',(req, res)=>{
+    const newProduct =req.body;
+    console.log('adding new product',newProduct);
+    productCollection.insertOne(newProduct)
+    .then(result => {
+        console.log('inserted count :',result.insertedCount);
+        res.send(result.insertedCount > 0)
+    })
+    
+})
+
+app.delete('/delete/:id',(req,res)=>{
+    const id=ObjectID(req.params.id)
+    console.log(id);
+    
+    productCollection.findOneAndDelete({_id:id})
+    .then(documents => {
+        console.log('documents deleted',documents);
+        res.send(!! documents.value);
+    })
+})
+
+//   client.close();
+
+});
+
+
+app.listen(port)
